dc6ce3e4b0c222953fcab5904b26f95813c502c5
Josh Waite <kalewaite@gmail.com>
2020-06-17 15:21:15 -0600

    initial commit

---
+++ src/mongodb.js
@@ -0,0 +1,11 @@
+const MongoClient = require('mongodb').MongoClient;
+
+module.exports = function (app) {
+  const connection = app.get('mongodb');
+  console.log(connection);
+  const database = connection.substr(connection.lastIndexOf('/') + 1);
+  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
+    .then(client => client.db(database));
+
+  app.set('mongoClient', mongoClient);
+};
